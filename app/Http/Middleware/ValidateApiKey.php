00703a781b280d5ee15990a43eb9968cd4b06613
FakhriAzraShafara <fakhrishafara@student.uns.ac.id>
2025-01-25 14:55:14 +0700

    Add ValidateApiKey middleware

--- app/Http/Middleware/ValidateApiKey.php
+++ app/Http/Middleware/ValidateApiKey.php
@@ -5,23 +5,19 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
 
 class ValidateApiKey
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        $apikey = $request->header('X-API-KEY');
+        $apiKey = $request->header('X-API-KEY');
 
-        if (!$apikey || $apikey !== config('app.api_key')) {
+        if (!$apiKey || $apiKey !== config('app.api_key')) {
             return response()->json([
-                'message'=>'Invalid Api Key'
+                'message' => 'Unauthorized: Invalid API Key'
             ], 401);
-        }
+        }        
         return $next($request);
     }
 }
